21985e64380aa5589ead5583004a6e1892b6b846
Khon Angkearith <ahritha@gmail.com>
2025-03-09 14:21:13 +0700

    log connection

--- FullStack-Project/api-pos-nit/src/util/connection.js
+++ FullStack-Project/api-pos-nit/src/util/connection.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql2/promise");
 const { config } = require("./config");
-module.exports = mysql.createPool({
+
+const pool = mysql.createPool({
   host: config.db.HOST,
   user: config.db.USER,
   password: config.db.PASSWORD,
@@ -8,3 +9,22 @@ module.exports = mysql.createPool({
   port: config.db.PORT,
   namedPlaceholders: true,
 });
+
+pool.getConnection()
+  .then(connection => {
+    console.log("MySQL connected successfully");
+    connection.release();  
+  })
+  .catch(error => {
+    console.error("Error connecting to MySQL:", error.message);
+  });
+
+pool.on('acquire', (connection) => {
+  console.log(`Connection ${connection.threadId} acquired`);
+});
+
+pool.on('release', (connection) => {
+  console.log(`Connection ${connection.threadId} released`);
+});
+
+module.exports = pool;
